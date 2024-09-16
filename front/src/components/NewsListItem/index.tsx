0d1866d2b49e20c4c9e2fde6bf44a033b5cbe227
junsu97 <wnstn7759@naver.com>
2024-09-16 14:44:37 +0900

    뉴스 리스트 아이템 컴포넌트 작성완료

---
+++ front/src/components/NewsListItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../BoarListItem/style.css';
+import NewsListDTO from "../../types/interface/news-list-item.interface";
+import {useNavigate} from "react-router-dom";
+import DefaultProfileImage from "../../assets/image/default-profile-image.png";
+
+interface Props {
+    newsListItem: NewsListDTO
+}
+
+export default function NewsListItem({newsListItem}: Props) {
+    const {subject, img, url, date, writer, contents} = newsListItem;
+    const navigator = useNavigate();
+
+    const onClickHandler = () => {
+        if(url){
+           window.open(url, '_blank');
+        }
+    }
+
+    return (
+        <div className='board-list-item' onClick={onClickHandler}>
+            <div className='board-list-item-main-box'>
+                <div className='board-list-item-top'>
+                    <div className='board-list-item-profile-box'>
+                        {/*<div className='board-list-item-profile-image'*/}
+                        {/*     style={{backgroundImage: `url(${writerProfileImage ? writerProfileImage : DefaultProfileImage})`}}></div>*/}
+
+                    </div>
+                    <div className='boadr-list-item-writer-box'>
+                        <div className='board-list-nickname'>{writer}</div>
+                        <div className='board-list-item-write-datetime'>{date}</div>
+                    </div>
+                </div>
+                <div className='board-list-item-middle'>
+                    <div className='board-list-item-title'>{subject}</div>
+                    <div className='board-list-item-content'>{contents}</div>
+
+                </div>
+                <div className='board-list-item-bottom'>
+                    {/*<div*/}
+                    {/*    className='board-list-item-counts'>{`댓글 ${commentCount} · 좋아요 ${favoriteCount} · 조회수 ${viewCount}`}</div>*/}
+                </div>
+            </div>
+            {img !== null && (
+                <div className='board-list-item-image-box'>
+                    <div className='board-list-item-image'
+                         style={{backgroundImage: `url(${img})`}}></div>
+                </div>
+            )}
+
+        </div>
+    )
+}
---
+++ front/src/types/interface/news-list-item.interface.ts
@@ -0,0 +1,8 @@
+export default interface NewsListDTO{
+    subject : string,
+    img : string,
+    url : string,
+    contents : string,
+    date : string,
+    writer : string
+}
